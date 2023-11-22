72cf4e523ddd6a0b1432ea766c2c865765ef8d24
Bence22 <79482639+Bence22@users.noreply.github.com>
2023-11-23 00:07:18 +0100

    diagram

--- list/kepzesek.php
+++ list/kepzesek.php
@@ -2,13 +2,60 @@
 $dbh = new PDO('mysql:host=localhost;dbname=highschool', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
 
-// Adatbázis lekérdezés
-$stmt = $dbh->query("SELECT nev FROM kepzes");
+$stmt = $dbh->query("SELECT nev, felveheto FROM kepzes");
 
-// Eredmény kiírása
 echo '<ul>';
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    echo '<li>' . $row['nev'] . '</li>';
+    echo '<li>' . $row['nev'] .  ' - Felveheto: ' . $row['felveheto'] '</li>';
 }
 echo '</ul>';
+
+$data = [];
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $data['labels'][] = $row['nev'];
+    $data['data'][] = $row['felveheto'];
+}
+
+echo '<script>';
+echo 'var chartData = ' . json_encode($data) . ';';
+echo '</script>';
+?>
+
+<div>
+<canvas id="myChart"></canvas>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
+<script>
+axios.post('get_chart_data.php')
+  .then(response => {
+    const chartData = response.data;
+
+    const ctx = document.getElementById('myChart');
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: chartData.labels,
+        datasets: [{
+          label: '# of Votes',
+          data: chartData.data,
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  })
+  .catch(error => {
+    console.error('Hiba történt az adatok lekérésekor:', error);
+  });
+
+</script>
 ?>
--- list/ponthatar.php
+++ list/ponthatar.php
@@ -2,10 +2,8 @@
 $dbh = new PDO('mysql:host=localhost;dbname=highschool', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
 
-// Adatbázis lekérdezés
 $stmt = $dbh->query("SELECT nev, minimum FROM kepzes");
 
-// Eredmény kiírása
 echo '<ul>';
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     echo '<li>' . $row['nev'] . ' - Minimum: ' . $row['minimum'] . '</li>';
